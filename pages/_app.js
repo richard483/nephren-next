172eb28bee914394e3ea9bfa89fc44e4665ac967
richard483 <richard.william483@gmail.com>
2022-10-31 10:37:31 +0700

    feat: added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import GlobalStyle from "../styles/GlobalStyles";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
       <GlobalStyle />
       <Component {...pageProps} />
-
+      <Analytics />
     </div>
   );
 }
